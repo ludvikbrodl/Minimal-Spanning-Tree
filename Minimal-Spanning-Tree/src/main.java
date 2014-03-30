ae281068352f14edf9f04252e32c1e4372458c50
jakkra <ijakkra@gmail.com>
2014-03-30 17:20:48 +0200

    New shit

--- Minimal-Spanning-Tree/src/main.java
+++ Minimal-Spanning-Tree/src/main.java
@@ -6,23 +6,25 @@ import java.util.TreeSet;
 import java.util.Vector;
 
 public class main {
+	
 	public static void main(String[] args) {
-
+		TreeSet<Edge> edges = new TreeSet<Edge>();
 		Edge e1 = new Edge("A", "B", 2);
 		Edge e2 = new Edge("A", "C", 1);
 		Edge e3 = new Edge("B", "D", 3);
 		Edge e4 = new Edge("C", "D", 4);
-		ArrayList<HashSet<String>> groups = new ArrayList<HashSet<String>>(); //Kanke något annat bättre än arrayList?
 
-		
-		TreeSet<Edge> edges = new TreeSet<Edge>();
-		 edges.add(e1);
-		 edges.add(e2);
-		 edges.add(e3);
-		 edges.add(e4);
-		 System.out.println(edges);
-		
+		edges = new TreeSet<Edge>();
+		edges.add(e1);
+		edges.add(e2);
+		edges.add(e3);
+		edges.add(e4);
+		System.out.println(edges);
+		KruskalAlgo result = new KruskalAlgo(edges);
+		System.out.println(result.getResult());
+
 	}
+
 }
 
 class Edge implements Comparable<Edge> {
@@ -51,4 +53,22 @@ class Edge implements Comparable<Edge> {
 	public String toString() {
 		return weight + ": " + cityA + " - " + cityB;
 	}
+
 }
+
+class KruskalAlgo {
+	TreeSet<Edge> edges;
+	ArrayList<HashSet<String>> groups;
+	
+	public KruskalAlgo(TreeSet<Edge> edges){
+		this.edges = edges;
+		groups = new ArrayList<HashSet<String>>(); // Annat än arrayList?
+		
+	}
+
+	public String getResult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
